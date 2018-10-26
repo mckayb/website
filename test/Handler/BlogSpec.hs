21b9de3fd8cf543d37f3a8362ee87268a3f8657d
McKay Broderick <mckay.db@gmail.com>
2018-10-25 19:16:00 -0600

    Working more on better tests

--- test/Handler/BlogSpec.hs
+++ test/Handler/BlogSpec.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Handler.BlogSpec (spec) where
 
 import TestImport
 import Faker.Internet
+import Database.Persist.Sql (fromSqlKey)
 
 spec :: Spec
 spec = withApp $ do
@@ -9,20 +12,43 @@ spec = withApp $ do
     it "Shows Coming Soon if there are not any posts" $ do
       get BlogR
 
-      htmlCount "article .post" 0
+      htmlCount "article.post" 0
       bodyContains "Coming soon!"
       statusIs 200
 
     it "Works if there is a single post" $ do
-      fakeEmail <- liftIO $ pack <$> email
-      currTime <- liftIO getCurrentTime
       role <- createRole "Admin"
-      user <- createUser role fakeEmail
-      blogPost <- createPost user "This is my post" currTime
+      em <- liftIO $ pack <$> email
+      time <- liftIO getCurrentTime
+      user <- createUser role em
+      post' <- createPost user "# Test" time
+      let idSel = pack $ "#post_" <> (show . fromSqlKey $ entityKey post')
 
       get BlogR
       statusIs 200
-      htmlCount "article .post" 1
+      htmlCount "article.post" 1
+      htmlCount "span.text-muted" 1
+
+      htmlAnyContain "h1" "Test"
+      htmlCount idSel 1
+
+      -- clickOn idSel
+      -- statusIs 200
+      -- htmlCount "article.blog-post" 1
 
     it "Works if there are multiple posts" $ do
-      assertEq "Test" True True
+      role <- createRole "Admin"
+      em <- liftIO $ pack <$> email
+      time <- liftIO getCurrentTime
+      user <- createUser role em
+      _ <- createPost user "# First" time
+      _ <- createPost user "# Second" time
+      _ <- createPost user "# Third" time
+
+      get BlogR
+      statusIs 200
+      htmlCount "article.post" 3
+      htmlCount "span.text-muted" 3
+      htmlAnyContain "h1" "First"
+      htmlAnyContain "h1" "Second"
+      htmlAnyContain "h1" "Third"
