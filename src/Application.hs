a58021a4e15e89d97d49909991a2504a869b968a
McKay Broderick <mckay.db@gmail.com>
2018-11-03 10:08:31 -0600

    Switched back to safe migrations

--- src/Application.hs
+++ src/Application.hs
@@ -20,7 +20,7 @@ module Application
 import Control.Monad.Logger                 (liftLoc, runLoggingT)
 import Database.Persist.Postgresql          (createPostgresqlPool, pgConnStr,
                                              pgPoolSize, runSqlPool
-                                            , runMigrationUnsafe
+                                            -- , runMigrationUnsafe
                                             )
 import Import
 import Language.Haskell.TH.Syntax           (qLocation)
@@ -85,8 +85,8 @@ makeFoundation appSettings = do
     (pgPoolSize $ appDatabaseConf appSettings)
 
   -- Perform database migration using our application's logging settings.
-  -- runLoggingT (runSqlPool (runMigration migrateAll) pool) logFunc
-  runLoggingT (runSqlPool (runMigrationUnsafe migrateAll) pool) logFunc
+  runLoggingT (runSqlPool (runMigration migrateAll) pool) logFunc
+  -- runLoggingT (runSqlPool (runMigrationUnsafe migrateAll) pool) logFunc
 
   -- Return the foundation
   return $ mkFoundation pool
