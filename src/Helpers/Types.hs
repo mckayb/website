cf34f697ae4094bc1db47fde008f740d7b45ed83
McKay Broderick <mckay.db@gmail.com>
2018-11-04 13:32:46 -0700

    More slight clean up

--- src/Helpers/Types.hs
+++ src/Helpers/Types.hs
@@ -10,5 +10,6 @@ module Helpers.Types where
 
 import ClassyPrelude.Yesod 
 
+-- | A convenient synonym for database access functions.
 type DB a = forall (m :: * -> *).
   (MonadIO m) => ReaderT SqlBackend m a
